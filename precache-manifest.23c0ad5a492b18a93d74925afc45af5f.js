fb4adffd207c7d71efc8c4d93bb115e7860ef968
VladStorozhenko <43917096+VladStorozhenko@users.noreply.github.com>
2020-07-16 17:43:04 +0300

    Updates

---
+++ precache-manifest.23c0ad5a492b18a93d74925afc45af5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d075237553d981030b5d05591591f84",
+    "url": "/uxuidesign/index.html"
+  },
+  {
+    "revision": "78405806c3bc1f2d7921",
+    "url": "/uxuidesign/static/css/main.3d50adac.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/uxuidesign/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uxuidesign/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78405806c3bc1f2d7921",
+    "url": "/uxuidesign/static/js/main.4601a7d7.chunk.js"
+  },
+  {
+    "revision": "9afbd5dd0500bf977756",
+    "url": "/uxuidesign/static/js/runtime-main.c1cf5559.js"
+  },
+  {
+    "revision": "2e0e9c011684014eb99c5825811bf240",
+    "url": "/uxuidesign/static/media/heart.2e0e9c01.png"
+  },
+  {
+    "revision": "5b2bc7f51942571c3f1cd2b75ea4e64f",
+    "url": "/uxuidesign/static/media/phone.5b2bc7f5.png"
+  },
+  {
+    "revision": "c792b4453d24a2ab844593045a3c3a75",
+    "url": "/uxuidesign/static/media/start-background.c792b445.svg"
+  }
+]);
